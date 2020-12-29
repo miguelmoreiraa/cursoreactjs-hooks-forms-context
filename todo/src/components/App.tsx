89c17282c30c8999b81758850f687d797ca9cbbc
miguelmoreirabandtec <miguel.moreira@bandtec.com.br>
2020-12-29 16:27:17 -0300

    💫 50% do curso feito

---
+++ todo/src/components/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from './NavBar';
+import TodoList from './TodoList';
+
+const App = () => {
+    return (
+        <div className="uk-container">
+            <NavBar></NavBar>
+            <TodoList></TodoList>
+        </div>
+    );
+}
+
+
+export default App;
---
+++ todo/src/components/TodoList.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Todo } from '../models/Todo';
+import TodoListItem from './TodoListItem';
+
+const TodoList = () => {
+    const todos: Todo[] = [
+        { id: 1, title: "Ir ao supermercado", done: false },
+        { id: 2, title: "Ir a academia", done: false }
+
+    ];
+    return (
+        <table className="uk-table">
+            <caption>Lista de Tarefas</caption>
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Tarefa</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    todos?.map(
+                        todo =>(<TodoListItem key={todo.id} todo={todo}></TodoListItem>)
+                    )
+                }
+            </tbody>
+        </table>
+    );
+}
+
+export default TodoList;
