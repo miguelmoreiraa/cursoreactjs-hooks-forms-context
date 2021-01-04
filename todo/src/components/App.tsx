acc2d2a715f794f2fa8b8d3f13cbcc046d3ae5b9
miguelmoreirabandtec <miguel.moreira@bandtec.com.br>
2021-01-04 17:46:39 -0300

    🚧 100% finalizado

--- todo/src/components/App.tsx
+++ todo/src/components/App.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import TodoContext from '../contexts/TodoContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './NavBar';
 import TodoList from './TodoList';
+import AddTodo from './AddTodo';
 
 const App = () => {
     return (
         <TodoContext>
-            <div className="uk-container">
+            <Router>
                 <NavBar></NavBar>
-                <TodoList></TodoList>
-            </div>
+                <div className="uk-container">
+                    <Switch>
+                        <Route path="/create">
+                            <AddTodo></AddTodo>
+                        </Route>
+                        <Route path="/">
+                            <h4>Minha lista de tarefas</h4>
+                            <TodoList></TodoList>
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
         </TodoContext>
     );
 }
--- todo/src/contexts/TodoContext.tsx
+++ todo/src/contexts/TodoContext.tsx
@@ -1,5 +1,6 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import { Todo } from '../models/Todo';
+import { get, save } from '../services/TodoService';
 import { TodoContextType } from './TodoContextType';
 
 
@@ -11,21 +12,26 @@ export const TodoContext = createContext<TodoContextType>({
 });
 
 const TodoProvider = (props: any) => {
-    const todos: Todo[] = [
-        { id: 1, title: "Ir ao supermercado", done: true },
-        { id: 2, title: "Ir a academia", done: false }
-    ]
+    const [todos, setTodos] = useState<Todo[]>(get)
+
+    useEffect(() => {
+        save(todos); // sempre que você quiser reagir a mudança na lista...
+    }, [todos]) // vou ficar olhando os TODO, toda vez que um TODO for alterado, vamos executar o useEffect e mandar salvar os nossos TODO
 
     const addTodo = (title: string) => {
-        console.log('Adicionou ' + title)
+        const todo: Todo = { id: todos.length + 1, title: title, done: false }
+        setTodos([...todos, todo]);
     }
 
     const removeTodo = (todo: Todo) => {
-        console.log('Removeu ' + todo.title)
+        const index = todos.indexOf(todo);
+        setTodos(todos.filter((_, i) => i !== index));
     }
 
     const toggle = (todo: Todo) => {
-        console.log('Alterou ' + todo.title)
+        const index = todos.indexOf(todo);
+        todos[index].done = !todo.done;
+        setTodos([...todos])
     }
 
     return (
